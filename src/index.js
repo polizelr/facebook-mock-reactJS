61deb20c5552d9cd62ed6f69d0683f623fead294
Rafaela Polizel <rafaela.polizel@pca.com.br>
2020-07-01 21:00:04 -0300

    Added first react component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const sum = (a, b) => alert(a + b);
+import React from 'react';
+import { render } from 'react-dom';
 
-sum(4,5);
+import App from './App';
+
+render(<App />, document.getElementById('app'));
